1703308a10b6f0a01acdd9caac460484f971ddfc
shikh <shikh@HP>
2023-03-08 18:37:23 +0530

    Bootstrapped with Vite and configured net core to work with it

--- RangManch_Client/ReactVite/src/App.tsx
+++ RangManch_Client/ReactVite/src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [test, setTest] = useState(""); 
 
+    
   return (
     <div className="App">
       <div>
@@ -15,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='text-lg'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
